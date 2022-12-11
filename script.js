684d851f79ab0f803bc455f4dfa272016a6dd766
shivam907 <shivamkaushal907@gmail.com>
2022-12-12 03:21:43 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar");
-    console.log($nav.height());
+    // console.log($nav.height());
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height() / 3);
   });
 });
@@ -85,5 +85,93 @@ responsive.forEach((i) => {
     el.preventDefault();
     responsive.forEach((i) => i.classList.remove("nocus"));
     i.classList.toggle("nocus");
+  });
+});
+
+let totalCost = 0;
+let selected = [];
+document.querySelectorAll(".type .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    selected[0] = i.textContent + " website";
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".pages .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    selected[1] = i.textContent + " pages";
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".style .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    selected[2] = i.textContent + "styling";
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".cms .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    if (i.textContent == "Not Required") {
+      // continue;
+      return;
+    } else {
+      selected[3] = i.textContent + "panel";
+    }
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".data .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    // selected[4] = i.textContent;
+    if (i.textContent == "Not Required") {
+      // continue;
+      return;
+    } else {
+      selected[4] = i.textContent;
+    }
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".seo .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    // selected[5] = i.textContent;
+    if (i.textContent == "Not Required") {
+      // continue;
+      return;
+    } else {
+      selected[5] = i.textContent;
+    }
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".copy .active").forEach((i) => {
+  i.addEventListener("click", () => {
+    selected[6] = i.textContent;
+    if (i.textContent == "Not Required") {
+      // continue;
+      return;
+    } else {
+      selected[6] = i.textContent + "Copywriting";
+    }
+    // console.log(i.textContent);
+  });
+});
+document.querySelectorAll(".yes .no").forEach((i) => {
+  i.addEventListener("click", () => {
+    selected[7] = i.textContent;
+    // console.log(i.textContent);
+  });
+});
+console.log(selected);
+document.querySelectorAll("button").forEach((i) => {
+  i.addEventListener("click", () => {
+    console.log("ys");
+    document.querySelector("summary").insertAdjacentHTML(
+      "beforeend",
+      `<div class="webSummary">
+            <p>Static Website</p>
+            <p>1000 rs</p>
+          </div>
+        </div>`
+    );
   });
 });
